7987b414f8001e2e71e75efd103ccf3acb87e529
lqs19317 <lorenzo.salles@escola.pr.gov.br>
2026-05-03 23:30:21 -0300

    Create testes-sqlite.sql

---
+++ testes-sqlite.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Clientes (
+    ClienteID INTEGER PRIMARY KEY,
+    Nome TEXT NOT NULL
+);
+CREATE TABLE Pedidos (
+    PedidoID INTEGER PRIMARY KEY,
+    ClienteID INTEGER NOT NULL,
+    DataPedido TEXT,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes (ClienteID)
+);
+INSERT INTO Clientes (Nome) VALUES ('Ana');
+INSERT INTO Clientes (Nome) VALUES ('Bruno');
+INSERT INTO Pedidos (ClienteID, DataPedido) VALUES (99, '2026-04-08');
